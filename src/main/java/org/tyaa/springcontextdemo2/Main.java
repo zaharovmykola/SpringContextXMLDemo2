d71830cf3ab5de498537283bc6af8ffc15dcb049
YuriiTrofimenko <tyaa@ukr.net>
2020-05-25 17:41:25 +0300

    upd to spring 4

--- src/main/java/org/tyaa/springcontextdemo2/Main.java
+++ src/main/java/org/tyaa/springcontextdemo2/Main.java
@@ -14,14 +14,14 @@ public class Main {
 		ApplicationContext context =
 				new ClassPathXmlApplicationContext("ApplicationContext.xml");
 		
-		Unit warriorUnit = (Unit) context.getBean("warrior_unit");
-		Unit peasantUnit = (Unit) context.getBean("peasant_unit");
+		//Unit warriorUnit = (Unit) context.getBean("warrior_unit");
+		//Unit peasantUnit = (Unit) context.getBean("peasant_unit");
 		
-		warriorUnit.show();
-		warriorUnit.testActions();
+		//warriorUnit.show();
+		//warriorUnit.testActions();
 		
-		peasantUnit.show();
-		peasantUnit.testActions();
+		//peasantUnit.show();
+		//peasantUnit.testActions();
 		
 		
 		/* Inheritance */
@@ -33,20 +33,20 @@ public class Main {
 		
 		/* Method injection */
 		
-		/*House house = (House) context.getBean("house");
+		/* House house = (House) context.getBean("house");
 		//Unit warriorUnit = house.createUnit();
 		Unit peasantUnit = house.createUnit();
 		Unit peasantUnit2 = house.createUnit();
 		System.out.println(peasantUnit);
 		System.out.println(peasantUnit2);
 		peasantUnit.testActions();
-		peasantUnit2.testActions();*/
+		peasantUnit2.testActions(); */
 		
 		
 		/* Bean pool */
 		
-		//WarriorPool wPool = (WarriorPool) context.getBean("warriorPool");
-		//wPool.testActions();
+		/*WarriorPool wPool = (WarriorPool) context.getBean("warriorPool");
+		wPool.testActions();*/
 		
 		/* Bean pool restricted using SpEL */
 		
@@ -56,9 +56,9 @@ public class Main {
 		
 		/* Autowire */
 		
-		/* Unit rusL3WarriorUnit = (Unit) context.getBean("russian_l3_warrior_unit");
+		Unit rusL3WarriorUnit = (Unit) context.getBean("russian_l3_warrior_unit");
 		rusL3WarriorUnit.show();
-		rusL3WarriorUnit.testActions(); */
+		rusL3WarriorUnit.testActions();
 	}
 
 }
